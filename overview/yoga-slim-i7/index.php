a6bd6fde88eb63056ee2c58cf25e9e0659e30600
Shayan <87204203+shayanafkane@users.noreply.github.com>
2021-09-25 11:04:44 +0330

    Update index.php

--- overview/yoga-slim-i7/index.php
+++ overview/yoga-slim-i7/index.php
@@ -1,1 +1,159 @@
 
+ 
+<!doctype html>
+<html lang="fa" dir="rtl">
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset') ?>"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php echo get_template_directory_uri(); ?>/src/landing-pages/aos.css
+<?php echo get_template_directory_uri(); ?>/src/landing-pages/bootstrap.min.css
+<?php echo get_template_directory_uri(); ?>/src/landing-pages/btn-animate.css
+<?php echo get_template_directory_uri(); ?>/src/landing-pages/bg-body.css
+<?php echo get_template_directory_uri(); ?>/src/landing-pages/font-awesome.css
+<?php echo get_template_directory_uri(); ?>/src/landing-pages/styles.css
+<?php echo get_template_directory_uri(); ?>/src/landing-pages/responsive.css
+    <?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PYPRDYXYKY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PYPRDYXYKY');
+</script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	
+</head>
+<body>
+<!-- Start content -->
+<div id="Body">
+        <div class="bg-body">
+            <div id="main">
+                <div class="box-landing">
+                    <div class="row">
+                        <div class="col-lg-6 box-landing-img">
+                            <img src="dist/img/1.png" class="img-fluid-custom" alt="">
+                        </div>
+                        <div class="col-lg-6 box-landing-title">
+
+                            <h1>
+                                لپ تاپ Yoga slim i7 pro
+                            </h1>
+                            <p class="text-rtl typography-title typography">در صنعت تکنولوژی، گاهی اوقات سازندگان از پسوند «Pro» به طوری استفاده می‌کنند تا کاربران را وادار به خرید محصولات خود کنند و معمولا چیز زیادی به محصولات خود اضافه نمی‌کنند. در این بین، سازندگانی هم هستند که به قولی که داده‌اند
+                                عمل می‌کنند و لنوو هم با لپ‌تاپ Yoga Slim 7i Pro به قول خود در قبال پسوند «Pro» عمل کرده است. در ادامه با بررسی لپ تاپ لنوو Yoga Slim 7i Pro همراه اعتبارکالا باشید.</p>
+
+                            <section class="container">
+                                <button data-hover="خرید"><div>خرید این محصول</div></button>
+                            </section>
+                        </div>
+                        <div class="mouse_scroll">
+
+                            <div class="mouse">
+                                <div class="wheel"></div>
+                            </div>
+                            <div>
+                                <span class="m_scroll_arrows unu"></span>
+                                <span class="m_scroll_arrows doi"></span>
+                                <span class="m_scroll_arrows trei"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="main-content">
+                        <div class="row">
+                            <div class="col-lg-6" data-aos="fade-right" data-aos-delay="150">
+                                <p class="text-rtl typography-title typography left-m-text">لپ‌تاپ لنوو Yoga Slim 7i Pro مورد بررسی ما دارای نمایشگر ۱۴ اینچی (۳۵.۵۶ سانتی‌متر) و وضوح تصویر ۱۸۰۰×۲۸۸۰ است. علاوه بر این‌ها، نسبت نمایشگر دستگاه ۱۶:۱۰و تراکم پیکسلی آن 243ppi است. این نمایشگر از فاصله حداقلی ۳۶ سانتی‌متری
+                                    مانند پنل رتینا (Retina) شرکت اپل است. یعنی از این فاصله، چشم انسان معمولی نمی‌تواند پیسکل‌ها را ببیند. زاویه دید نمایشگر این لپ‌تاپ نیز عالی است. در زیر تصاویر نمایشگر دستگاه در زاویه ۴۵ درجه را مشاهده می‌کنید.</p>
+                            </div>
+                            <div class="col-lg-6" data-aos="fade-left">
+                                <div class="picture-cam">
+                                    <img src="dist/img/2.png" class="img-custom-cam" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="main-content">
+                        <div class="row">
+                            <div class="col-lg-6 picture-3" data-aos="fade-right" data-aos-delay="150">
+                                <img src="dist/img/3.png" class="img-custom-cam" alt="">
+                            </div>
+                            <div class="col-lg-6" data-aos="fade-left">
+                                <div class="">
+                                    <p class="text-rtl right-m-text  m-text-right ">لپ‌تاپ یوگا Slim 7i Pro بسته‌بندی زیبا و فوق‌العاده‌ای دارد و یکی از جالب‌ترین ویژگی‌های آن نمایشگر ۱۴ اینچی با نسبت ۱۶ به ۱۰ است. لپ‌تاپ مورد بررسی دارای رزولوشن ۲۸۸۰×۱۸۰۰ با کیفیت 2.8K است؛ اما این دستگاه در رزولوشن
+                                        ۲۲۴۰×۱۴۰۰ با کیفیت 2.2K نیز عرضه می‌شود. نمایشگر این لپ‌تاپ از فناوری IPS بهره می‌برد و ۱۰۰ درصد طیف رنگی sRGB را نیز نمایش می‌دهد. به راحتی می‌توان این دو را از یکدیگر تشخیص داد؛ چرا که نسخه 2.2K نمایشگر مات و
+                                        ضد تابش نور دارد و مدل دومی دارای نمایشگر براق است. با این حال، بزرگ‌ترین تفاوت این دو نمایشگر از نظر تجربه کاربری، نرخ نوسازی صفحه است که نسخه مورد بررسی دارای نرخ نوسازی ۹۰ هرتز و مدل دوم دارای نرخ استاندارد ۶۰
+                                        هرتز است. بیایید کمی عمیق‌تر به این لپ‌تاپ زیبا نگاه کنیم و وارد بررسی تخصصی آن شویم.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-content">
+                            <div class="row">
+                                <div class="col-lg-6" data-aos="fade-right" data-aos-delay="150">
+                                    <p class="text-rtl typography-title typography left-m-text">نمایشگر ۱۴ اینچی لنوو Yoga Slim 7i Pro با وضوح بالای 2.8K، زاویه دید مناسب، حداکثر روشنایی بالا و نسبت کنتراست عالی، خیال شما را از یک نمایشگر خوب راحت می‌سازد و به راحتی می‌توانید کارهای گرافیکی خود را بدون از دست
+                                        دادن کیفیت تصویر انجام دهید. حتی می‌توانید با نصب پروفایل‌های Gaming و Web design، عملکرد این نمایشگر را برای تولید محتوا و امور کاری خود بهبود ببخشید. به علاوه، با داشتن نرخ نوسازی ۹۰ هرتز، جابجایی بین صفحات در
+                                        این لپ‌تاپ به نرمی انجام می‌شود.</p>
+                                </div>
+                                <div class="col-lg-6" data-aos="fade-left">
+                                    <div class="picture-5">
+                                        <img src="dist/img/4.png" class="picture-55" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="container">
+
+                            <h1 class="title-battery">باتری</h1>
+                            <div class="row">
+                                <div class="col-lg-3 col-sm-6 text-center battery-box bt-box" style="margin-top: -15px;" data-aos="fade-up" data-aos-delay="100">
+                                    <i class="fa fa-battery-full icone-battery" aria-hidden="true"></i>
+                                    <h2>61 وات ساعت</h2>
+                                </div>
+                                <div class="col-lg-3 col-sm-6 text-center battery-box " data-aos="fade-up" data-aos-delay="200">
+
+                                    <i class="fa fa-tachometer" aria-hidden="true"></i>
+                                    <h2>1 ساعت شارژ کامل</h2>
+                                </div>
+                                <div class="col-lg-3 col-sm-6 text-center battery-box " data-aos="fade-up" data-aos-delay="300">
+                                    <i class="fa fa-desktop" aria-hidden="true"></i>
+                                    <h2>11 ساعت فیلم</h2>
+                                </div>
+                                <div class="col-lg-3 col-sm-6 text-center battery-box " data-aos="fade-up" data-aos-delay="400">
+                                    <i class="fa fa-internet-explorer" aria-hidden="true"></i>
+                                    <h2>12 ساعت وب گردی</h2>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <h1 class="title-battery">رنگ بندی </h1>
+                            <div class="collor-phone">
+                                <h2 class="h2c-1" data-aos="fade-right" data-aos-delay="100">نقره ای</h2>
+                                <h2 class="h2c-3" data-aos="fade-right" data-aos-delay="200">خاکستری</h2>
+                            </div>
+                            <div class=" row ">
+                                <div class="col text-center " data-aos="fade-up" data-aos-delay="700">
+                                    <img src="dist/img/5.png " class="picture-4" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <button data-hover="خرید " class="button-price "><div>خرید این محصول</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+<!-- end content -->
+ <?php wp_footer() ?>
+
+
+    <?php echo get_template_directory_uri(); ?>/src/landing-pages/https://use.fontawesome.com/386bd41aef.js 
+    <?php echo get_template_directory_uri(); ?>/src/landing-pages/https://code.jquery.com/jquery-1.11.3.min.js
+    <?php echo get_template_directory_uri(); ?>/src/landing-pages/aos.js
+    <?php echo get_template_directory_uri(); ?>/src/landing-pages/bootstrap.min.js
+</body>
+</html>
